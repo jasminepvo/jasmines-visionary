e5de34255ec3db4a86520ce3737b8090807e6e11
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-20 15:43:26 -0400

    Add logo and create images to export as object

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { images } from "../../constants";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav>
+			<div>
+				<img src={images.logo} alt="logo" />
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
---
+++ frontend-react/src/constants/images.js
@@ -0,0 +1,5 @@
+import logo from "../assets/jv-visionary-logo-clear.png";
+
+export default {
+	logo,
+};
