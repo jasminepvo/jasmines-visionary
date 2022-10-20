4d14de368a99e1c46020a9844babe35e4a4d0f50
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-20 15:38:04 -0400

    Add customized styles to tailwind - font, colors

--- frontend-react/tailwind.config.cjs
+++ frontend-react/tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
+		colors: {
+			taupe: "#AD9D90",
+			charcoal: "#545454",
+		},
+		fontFamily: {
+			Abril: ["Abril Fatface", cursive],
+			DM: ["DM Serif Display", serif],
+		},
 		extend: {},
 	},
 	plugins: [],
