a49bb901f1563b969d770e1f92fb7e6f92d2d5b2
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-20 15:55:57 -0400

    Create navbar with logo

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { images } from "../../constants";
+import { About } from "../../container";
 
 const Navbar = () => {
 	return (
 		<nav>
 			<div>
-				<img src={images.logo} alt="logo" />
+				<img className="h-40 w-40 object-cover" src={images.logo} alt="logo" />
 			</div>
+			<ul>
+				{["home", "about", "work", "skills"].map((item) => (
+					<li className="flex justify-center" key={`link-${item}`}>
+						<div />
+						<a href={`#${item}`}>{item}</a>
+					</li>
+				))}
+			</ul>
 		</nav>
 	);
 };
--- frontend-react/tailwind.config.cjs
+++ frontend-react/tailwind.config.cjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		colors: {
 			taupe: "#AD9D90",
 			charcoal: "#545454",
 		},
 		fontFamily: {
-			Abril: ["Abril Fatface", cursive],
-			DM: ["DM Serif Display", serif],
+			Abril: ["Abril Fatface", "cursive"],
+			DM: ["DM Serif Display", "serif"],
 		},
 		extend: {},
 	},
