803d4b366512105c1e85e40faa9efe05718ac1cd
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-20 15:19:19 -0400

    Create components and containers

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { About, Footer, Header, Work } from "./container";
+import { Navbar } from "./components";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="">
+			<Navbar />
+			<Header />
+			<About />
+			<Work />
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
---
+++ frontend-react/src/container/index.js
@@ -0,0 +1,4 @@
+export { default as About } from "./About/About";
+export { default as Footer } from "./Footer/Footer";
+export { default as Header } from "./Header/Header";
+export { default as Work } from "./Work/Work";
