1dafb682c19098f5a17ac35f3b9dc9a099e931b2
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-21 20:04:08 -0400

    Add navbar component

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { images } from "../../constants";
-import { About } from "../../container";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+	const [toggle, setToggle] = useState(false);
+
 	return (
-		<nav>
-			<div>
-				<img className="h-40 w-40 object-cover" src={images.logo} alt="logo" />
+		<nav className="flex w-full justify-between items-center fixed px-8 py-4 backdrop-blur">
+			<div className="flex justify-start">
+				<img className="h-20 w-25" src={images.logo} alt="logo" />
 			</div>
-			<ul>
+			<ul className="hidden md:contents flex-1 flex justify-center items-center">
 				{["home", "about", "work", "skills"].map((item) => (
-					<li className="flex justify-center" key={`link-${item}`}>
-						<div />
+					<li
+						className="mx-4 cursor-pointer flex-col uppercase"
+						key={`link-${item}`}
+					>
 						<a href={`#${item}`}>{item}</a>
 					</li>
 				))}
 			</ul>
+
+			{/* Hamburger Menu */}
+			<div className="flex justify-center items-center">
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
+
+				{toggle && (
+					<motion.div
+						whileInView={{ x: [300, 0] }}
+						transition={{ duration: 0.85, ease: "easeOut" }}
+					>
+						<HiX onClick={() => setToggle(false)} />
+						{["home", "about", "work", "skills"].map((item) => (
+							<li
+								// className="fixed z-5 w-4/5 h-screen flex justify-end items-end flex-col bg-charcoal"
+								key={item}
+							>
+								<div className="fixed z-5 p-4 w-4/5 h-screen flex justify-end items-end flex-col bg-[url('../../assets/bgWhite.png')] bg-white bg-cover bg-repeat" />
+								<a href={`#${item}`} onClick={() => setToggle(false)}>
+									{item}
+								</a>
+							</li>
+						))}
+					</motion.div>
+				)}
+			</div>
 		</nav>
 	);
 };
