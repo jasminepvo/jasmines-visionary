579d9b3aae74f9a076614dfdc6d9cfd7154fc7ba
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-24 19:35:15 -0400

    Add skills component

--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -16,24 +16,30 @@ const Skills = () => {
 	});
 
 	return (
-		<div className="flex justify-center items-center bg-bgWhite">
-			<h2>Skills</h2>
-			<div className="skillscontainer">
-				<motion.div className="appskillslist">
+		<div className="bg-bgWhite flex-1 w-full flex-col flex justify-center items-center">
+			<h2 className="text-4xl font-extrabold text-taupe uppercase">
+				Whats my tech stack?
+			</h2>
+			<div className="w-4/5 mt-12 flex flex-row md:w-full md:flex-col">
+				<motion.div className="flex-1 flex flex-wrap justify-center items-center mx-10 md:mx-24 lg:mx-28">
 					{skills.map((skill) => (
 						<motion.div
-							className="flex justify-center items-center appskills-item"
+							className="flex justify-center items-center flex-col text-center m-4 transition-all"
 							whileInView={{ opacity: [0, 1] }}
 							transition={{ duration: 0.5 }}
 							key={skill.name}
 						>
 							<div
-								className="flex justify-center items-content"
+								className="flex justify-center items-center w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 bg-white rounded-full hover:shadow-taupe hover:shadow-sm"
 								style={{ backgroundColor: skill.bgColor }}
 							>
-								<img src={urlFor(skill.icon)} alt={skill.name} />
+								<img
+									className="w-4/5 h-4/5 hover:animate-[spin_1s_infinite]"
+									src={urlFor(skill.icon)}
+									alt={skill.name}
+								/>
 							</div>
-							<p>{skill.name}</p>
+							<p className="mt-4 lg:mt-8">{skill.name}</p>
 						</motion.div>
 					))}
 				</motion.div>
