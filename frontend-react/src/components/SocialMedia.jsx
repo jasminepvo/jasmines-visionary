dcc3cfae561e5dffc02dad2f0e5b91619029ce9b
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-24 20:26:44 -0400

    Add links to social media icons

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -6,13 +6,19 @@ const SocialMedia = () => {
 	return (
 		<div className="hidden lg:flex justify-end items-end left-4 flex-col p-4 z-50 absolute">
 			<div className="w-10 h-10 rounded-full bg-white my-1 mx-0 border-charcoal border-1 flex justify-center items-center hover:bg-taupe">
-				<FaLinkedinIn />
+				<a href="https://linkedin.com/in/jasminepvo">
+					<FaLinkedinIn className="fill-taupe hover:fill-white" />
+				</a>
+			</div>
+			<div className="w-10 h-10 rounded-full bg-white my-1 mx-0 border-charcoal border-1 flex justify-center items-center hover:bg-taupe ">
+				<a href="https://github.com/jasminepvo">
+					<BsGithub className="fill-taupe hover:fill-white" />
+				</a>
 			</div>
 			<div className="w-10 h-10 rounded-full bg-white my-1 mx-0 border-charcoal border-1 flex justify-center items-center hover:bg-taupe">
-				<BsGithub />
-			</div>
-			<div className="w-10 h-10 rounded-full bg-white my-1 mx-0 border-charcoal border-1 flex justify-center items-center hover:bg-taupe">
-				<BsTwitter />
+				<a href="https://twitter.com/jasminepvodev">
+					<BsTwitter className="fill-taupe hover:fill-white" />
+				</a>
 			</div>
 		</div>
 	);
