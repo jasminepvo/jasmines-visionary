402b67400d44645055bcc64b3caf5248f0aafd77
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2024-01-03 17:18:04 -0600

    Update Deprecated Default Export in @sanity/client:

--- frontend-react/src/client.js
+++ frontend-react/src/client.js
@@ -1,12 +1,14 @@
-import sanityClient from "@sanity/client";
+// Updated import statement
+import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const client = sanityClient({
-	projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-	dataset: "production",
-	apiVersion: "2022-02-01",
-	useCdn: true,
-	token: import.meta.env.VITE_SANITY_TOKEN,
+// Updated client initialization
+export const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // Ensure these values are correct
+  dataset: "production",
+  apiVersion: "2022-02-01",
+  useCdn: true,
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
