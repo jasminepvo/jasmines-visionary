4f431dfb39d04a99f00440cf2f46adc0532563a1
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2024-02-12 15:46:56 -0600

    Remove footer

--- frontend-react/src/container/Footer/Footer.jsx
+++ frontend-react/src/container/Footer/Footer.jsx
@@ -4,24 +4,28 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { IoIosCopy } from "react-icons/io";
 
 const Footer = () => {
-	// state = {
-	// 	value: "jasminepvo.dev@gmail.com",
-	// 	copied: false,
-	// };
-
-	return (
-		<div className="flex justify-center items-center flex-col bg-bgWhite pt-16">
-			<p className="text-charcoal">Send any inquiries to: </p>
-			<CopyToClipboard
-				className="pb-8 flex flex-row hover:underline text-charcoal hover:text-taupe "
-				text="jasminepvo.dev@gmail.com"
-			>
-				<a href="mail:jasminepvo.dev@gmail.com">
-					jasminepvo.dev@gmail.com <IoIosCopy />
-				</a>
-			</CopyToClipboard>
-		</div>
-	);
+  return (
+    <div class="flex-1 w-full mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4 bg-bgWhite border-1">
+      <a
+        href="#"
+        class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-taupe rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+      >
+        Learn more
+        <svg
+          class="ml-2 -mr-1 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
